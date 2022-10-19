be621f4b2029905d97ee15451116082d587b2247
Edivaldo <edivaldosantosjunior2004@gmail.com>
2022-10-19 18:20:31 -0300

    Lista de Tarefas

--- js/main.js
+++ js/main.js
@@ -5,52 +5,24 @@ window.onload = () => {
     }
 };
 
-function calcular() {
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    let nome = document.getElementById("nome").value;
-    let idade = document.getElementById("idade").value;
-    let sexo = document.getElementById("sexo").value;
+function salvar() {
 
-    let result = peso / (altura * altura)
-    document.getElementById("result").innerHTML = Math.floor(result);
-    imprimir_nome.textContent = "Nome: " + nome;
-    imprimir_idade.textContent = "Idade: " + idade + " anos";
-    imprimir_sexo.textContent = "Sexo: " + sexo;
-    imprimir_altura.textContent = "Altura: " + altura + " cm";
-    imprimir_peso.textContent = "Peso: " + peso + " Kg";
+    let textarea = document.getElementById("textarea").value;
+    let date = document.getElementById("date").value;
+    let lista = document.getElementById("lista").value;
+    
+    let imprimir_descri = document.getElementById("imprimir_descri").innerHTML = "Tarefa: " + textarea;
 
-    if(result < 18.5){
-        document.getElementById("msg").innerHTML = "Abaixo do peso normal";
-    }else if(result > 18.5 & result < 24.9){
-        document.getElementById("msg").innerHTML = "Peso Normal";
-    }else if(result > 25.0 & result < 29.9){
-        document.getElementById("msg").innerHTML = "Excesso de Peso";
-    }else if(result > 30.0 & result <= 34.9){
-        document.getElementById("msg").innerHTML = "Obesidade classe 1";
-    }else if(result >= 35.0 & result <= 39.9){
-        document.getElementById("msg").innerHTML = "Obesidade classe 2";
-    }else if(result >= 40.0){
-        document.getElementById("msg").innerHTML = "Obesidade classe 3";
+    let imprimir_data = document.getElementById("imprimir_data").innerHTML = "Data da Tarefa: " + date;
+
+    for(let salvar of imprimir_descri) {
+        let item = document.createElement("li");
+        let texto = document.createElement(lista + ' - ');
+        let botao = document.createElement("button");
+        botao.innderText = 'dffdfd';
+
+        item.appendChild(texto);
+        item.appendChild(botao);
+        item.appendChild(imprimir_descri);
     }
-}
-function Limpar(){
-    let peso = document.getElementById("peso").value = "";
-    let altura = document.getElementById("altura").value = "";
-    let nome = document.getElementById("nome").value = "";
-    let idade = document.getElementById("idade").value = "";
-    let sexo = document.getElementById("sexo").value = "";
-    result.innerHTML = "";
-}
-function PDF() {
-    var pegar_dados = document.getElementById("final").innerHTML;
-
-    var janela = window.open('', '', 'width=800, height=600');
-    janela.document.write('<html><head>');
-    janela.document.write('<title>PDF</title>');
-    janela.document.write('<body>');
-    janela.document.write(pegar_dados);
-    janela.document.write('</body></html>');
-    janela.document.close();
-    janela.print();
 }
